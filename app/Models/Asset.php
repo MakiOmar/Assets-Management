626525e8a87d21ba30b538f3831c6aabde31b7db
makiomar <mo7amed.maki@gmail.com>
2025-01-17 20:45:51 +0200

    Get methods

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Asset extends Model
 {
@@ -40,5 +41,31 @@ class Asset extends Model
             $this->value -= $amount;
             $this->save();
         }
+    }
+
+    public static function getRecentByUser($userId, $limit = 10)
+    {
+        return self::where('user_id', $userId)->orderBy('date', 'desc')->take($limit)->get();
+    }
+
+    public static function getByUser($userId, $perPage = 10)
+    {
+        return self::where('user_id', $userId)
+        ->orderBy('created_at', 'desc')
+        ->paginate($perPage);
+    }
+
+    public static function getAssets($request)
+    {
+        // Check if the user is an admin
+        if ($request->user()->hasRole('administrator')) {
+            // Admin: Get all transactions with pagination (10 per page)
+
+            $transactions = self::orderBy('created_at', 'desc')->paginate(10);
+        } else {
+            // Non-admin: Get transactions only for the logged-in user with pagination (10 per page)
+            $transactions = self::getByUser($request->user()->id, 10);
+        }
+        return $transactions;
     }
 }
